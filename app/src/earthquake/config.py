7f71c14cbcaf50f19f4ec1dc8008868b563f8845
goppy <tacumin.goppy.06@gmail.com>
2020-07-18 19:22:03 +0900

    情報追加

--- app/src/earthquake/config.py
+++ app/src/earthquake/config.py
@@ -2,3 +2,6 @@
 # -*- coding: utf-8 -*-
 
 jma_eqvol_url = 'http://www.data.jma.go.jp/developer/xml/feed/eqvol.xml'
+eqvol_title   = "震源・震度に関する情報"
+detail_title  = "震源・震度に関する情報"
+quick_title   = "震度速報"
