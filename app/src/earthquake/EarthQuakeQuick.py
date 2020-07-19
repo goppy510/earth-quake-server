4574df417c5d67f59e9e3b0aa3dc5a1ec435bcda
goppy <tacumin.goppy.06@gmail.com>
2020-07-19 14:28:06 +0900

    修正

--- app/src/earthquake/EarthQuakeQuick.py
+++ app/src/earthquake/EarthQuakeQuick.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from . import config
-import EarthQuakeCommon
+from . import EarthQuakeCommon
 import datetime
 import xml.etree.ElementTree as ET
 import pprint
 import time
 import urllib.request
 
+
 class EarthQuakeQuick:
     def __init__(self):
         self.__target_title = config.detail_title
-        self.eq_common = EarthQuakeCommon(self.__target_title)
-        self.xml_url = self.eq_common.get_xml_url()
+        self.__eq_c = EarthQuakeCommon.EarthQuakeCommon(self.__target_title)
+        self.__xml_url = self.__eq_c.get_xml_url()
         self.__pref_row = 2
         self.__area_row = 3
 
@@ -22,12 +23,12 @@ class EarthQuakeQuick:
 
         # テスト用
         url = 'http://www.data.jma.go.jp/developer/xml/data/361b530f-d7db-3d79-a4e3-a8191de8c47a.xml'
-        parsed = self.eq_common.parse_url(self.xml_url)
-        parsed = self.eq_common.parse_url(url)
+        parsed = self.__eq_c.parse_url(self.__xml_url)
+        parsed = self.__eq_c.parse_url(url)
 
         head = parsed[1]
         event_id   = head[3].text
-        event_time = self.eq_common.parse_time_str(str(event_id)) # 発生時刻取得
+        event_time = self.__eq_c.parse_time_str(str(event_id)) # 発生時刻取得
         result["title"]      = head[0].text # title取得
         result["event_time"] = event_time
 
