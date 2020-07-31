f4e126c580c6aa7760f321f0ff4a023771b31287
Takumi Goto <34117290+goppy510@users.noreply.github.com>
2020-07-31 09:47:23 +0900

    緊急地震速報取得スクリプト作成 (#6)
    * 緊急地震速報取得スクリプト作成
    
    * jsonがdictのままだったのでそれを取得するように修正
    
    * テスト用のエンドポイント作成
    
    * slack通知に予測震度の条件を追加

---
+++ eew/Eew.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import requests
+
+
+class Eew():
+    def __init__(self):
+        self.__common_uri = 'http://www.kmoni.bosai.go.jp/webservice/hypo/eew/'
+
+
+    def get_json_data(self):
+        endpoint = self.__create_endpoint()
+        json_data = requests.get(endpoint).json()
+        return json_data
+
+
+    def __create_endpoint(self):
+        extension = '.json'
+        nowtime  = self.__get_current_date()
+        endpoint = self.__common_uri + str(nowtime) + extension
+        # テスト用
+        # endpoint = self.__common_uri + str(20200730093733) + extension
+        return endpoint
+
+
+    def __get_current_date(self):
+        date_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        return date_time
---
+++ eew/slack.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import config
+import slackweb
+import sys, os
+import json
+import requests
+
+
+class Slacks():
+    def __init__(self):
+        self.__token  = config.incoming_webhook_url
+        self.__minint = 2
+        # 以下はslack系
+        self.__title = "緊急地震速報"
+        self.__icons = ["one", "two", "three", "four", "five", "six", "seven"]
+
+
+    def post_slack(self, json_data):
+        __json_data = json_data
+        hypo_type   = str(json_data['request_hypo_type'])
+        report_num  = str(json_data['report_num'])
+        intensity   = int(str(json_data['calcintensity'])[:1])
+        if ('alertflg' in json_data.keys()):
+            alertflg    = str(json_data['alertflg'])
+            user_name = self.__title + '(' + alertflg + ')'
+            body      = self.__create_body(__json_data)
+            icon      = self.__create_icon(__json_data)
+            # 緊急地震速報かつ第一報かつ予想震度が2以上ならslackに通知する
+            if hypo_type == 'eew' and ((report_num == '1' and intensity >= 2) or alertflg =='警報'):
+                slack_conn = slackweb.Slack(url=self.__token)
+                slack_conn.notify(text=body, username=user_name, icon_emoji=icon)
+
+
+    def __create_body(self, json_data):
+        origin_time   = self.__parse_time(str(json_data['origin_time']))
+        region_name   = str(json_data['region_name'])
+        depth         = str(json_data['depth'])
+        magunitude    = float(json_data['magunitude'])
+        latitude      = float(json_data['latitude'])
+        longitude     = float(json_data['longitude'])
+        calcintensity = str(json_data['calcintensity'])
+        is_cancel     = bool(json_data['is_cancel'])
+
+        # 本文のパーツ作成
+        body_mention       = "<!here>" + "\n\n"
+        body_origin_time   = "地震発生時刻： " + " *" + origin_time + "* \n\n"
+        body_calcintensity = "推定最大震度： " + " *" + calcintensity + "* \n\n"
+        body_magunitude    = "マグニチュード： M" + " *" + str(magunitude) + "* \n\n"
+        body_region        = "震央地： " +  region_name + "\n"
+        body_coor          = "経緯度： " + "東経 " + str(longitude) + " 北緯 " + str(latitude) + "\n"
+        body_depth         = "震源の深さ： " + depth + "\n\n"
+        body               = body_mention + body_origin_time + body_calcintensity + body_magunitude + body_region + body_coor + body_depth
+        return body
+
+
+    # YYYYMMDDhh24msをYYYY/MM/DD hh24:mm:ssに変換する
+    def __parse_time(self, date):
+        dt = date[:4] + "/" + date[4:6] + "/" + date[6:8] + " " + date[8:10] + ":" + date[10:12] + ":" + date[12:14]
+        return dt
+
+
+    # slackアイコン
+    def __create_icon(self, json_data):
+        intensity = str(json_data['calcintensity'])
+        icon = ":" + self.__icons[int(intensity[:1])-1] + ":"
+        return icon
