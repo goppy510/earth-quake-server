0c5d8ca0ab6189479fa37703f3031293fa6e4ac2
goppy <tacumin.goppy.06@gmail.com>
2020-09-16 21:07:49 +0900

    add line call

--- eew/main.py
+++ eew/main.py
@@ -3,14 +3,17 @@
 
 from datetime import datetime
 from slack import Slacks
+from line import Lines
 from Eew import Eew
 
 
 def main():
     eew = Eew()
     slacks = Slacks()
+    lines = Lines()
     json_data = eew.get_json_data()
     slacks.post_slack(json_data)
+    lines.post(json_data)
 
 
 if __name__ == '__main__':
