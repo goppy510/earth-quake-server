9ec8b284be3f4e8ee073b43ba1bae9ea82b7e8fa
goppy <tacumin.goppy.06@gmail.com>
2020-09-16 21:06:46 +0900

    add line.py

--- app/src/line.py
+++ app/src/line.py
@@ -2,38 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import config
-import slackweb
+import requests
 import sys, os
 from earthquake import EarthQuakeDetail
 from earthquake import EarthQuakeQuick
 
-class Slacks():
+class Lines():
     def __init__(self):
-        self.__token  = config.goppy_slack_url
+        self.__token  = config.line_token
         self.__minint = 2
         self.__detail_title = "震源・震度に関する情報"
         self.__quick_title  = "震度速報"
-        # 以下はslack系
-        self.__icons = ["one", "two", "three", "four", "five", "six", "seven"]
+        self.__headers = {'Authorization': 'Bearer ' + self.__token}
 
 
     def post(self, data):
+        api_url = self.__get_api_url()
         if (data == False):
             return False
         title = data["title"]
         maxint = self.__parse_maxint(data["maxint"])[:1]
         body = None
         if (title == self.__detail_title):
-            body = self.__create_slack_body_detail(data)
+            body = self.__create_line_body_detail(data)
         if (title == self.__quick_title):
-            body = self.__create_slack_body_quick(data)
-        user_name = self.__create_slack_username(title)
-        slack_conn = slackweb.Slack(url=self.__token)
+            body = self.__create_line_body_quick(data)
+        message = title + "\n\n" + body
+        payload = {
+            'message': message,
+        }
         if int(maxint) >= self.__minint:
-            icon = self.__create_slack_icon(maxint)
-            slack_conn.notify(text=body, username=user_name, icon_emoji=icon)
-        else:
-            slack_conn.notify(text=body, username=user_name)
+            requests.post(api_url, headers=self.__headers, data=payload)
+
+
+    #Line APIのURL
+    def __get_api_url(self):
+        API_URL = 'https://notify-api.line.me/api/notify'
+        return API_URL
 
 
     # 最大震度パース
@@ -65,8 +70,8 @@ class Slacks():
         return maxint_city
 
 
-    # 震源・震度に関する情報のslack本文
-    def __create_slack_body_detail(self, data):
+    # 震源・震度に関する情報のline本文
+    def __create_line_body_detail(self, data):
         maxint_city = self.__create_area_list(data)
         hypo_area   = data["hypocenter"]["name"]
         coor_pt     = data["hypocenter"]["coordinate"]
@@ -79,8 +84,8 @@ class Slacks():
         if not maxint_city:
             maxint_city = "不明"
         body_event_time = "推定発生時刻： " + data["event_time"] + "\n\n"
-        body_maxint     = "最大震度： " + " *" + str(max_int) + "* \n\n"
-        body_mag        = "マグニチュード： M" + " *" + str(data["magnitude"]) + "* \n\n"
+        body_maxint     = "最大震度： " + str(max_int) + "\n\n"
+        body_mag        = "マグニチュード： M" + str(data["magnitude"]) + "\n\n"
         body_hypo       = "震央地： " +  hypo_area + "\n"
         body_coor       = "経緯度： " + "東経 " + east_pt + " 度・北緯 " + north_pt + " 度 \n"
         body_depth      = "震源の深さ： " + depth + "km\n\n"
@@ -89,8 +94,8 @@ class Slacks():
         body            = body_event_time + body_maxint + body_mag + body_hypo + body_coor + body_depth + body_area + body_area_name
         return body
 
-    # 震度速報のslack本文
-    def __create_slack_body_quick(self, data):
+    # 震度速報のline本文
+    def __create_line_body_quick(self, data):
         maxint_city = self.__create_area_list(data)
         max_int = self.__parse_maxint(data["maxint"])
         if not max_int:
@@ -98,20 +103,8 @@ class Slacks():
         if not maxint_city:
             maxint_city = "不明"
         body_event_time = "推定発生時刻： " + data["event_time"] + "\n\n"
-        body_maxint     = "最大震度： " + " *" + str(max_int) + "* \n\n"
+        body_maxint     = "最大震度： " + str(max_int) + "\n\n"
         body_area       = "震度"+ str(self.__minint) + "以上の観測地域： \n\n"
         body_area_name  = maxint_city + "\n"
         body            = bbody_event_time + body_maxint + body_area + body_area_name
         return body
-
-
-    # slackアイコン
-    def __create_slack_icon(self, maxint):
-        icon = ":" + self.__icons[int(maxint[:1])-1] + ":"
-        return icon
-
-
-    # slackのユーザー名
-    def __create_slack_username(self, title):
-        user_name = title
-        return user_name
